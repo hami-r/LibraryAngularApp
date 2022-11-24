e49426e9e22fd20f537583d0199b79b5a679b749
hami-r <mynameissaitama54@gmail.com>
2022-11-24 21:50:45 +0530

    view book added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { ViewBookComponent } from './view-book/view-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
     UserLoginComponent,
-    UserRegisterComponent
+    UserRegisterComponent,
+    ViewBookComponent
   ],
   imports: [
     BrowserModule,
