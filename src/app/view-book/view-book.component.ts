6c7d9e76a53a084c531c195140c70dd417976412
hami-r <mynameissaitama54@gmail.com>
2022-11-25 12:03:39 +0530

    data binding

--- src/app/view-book/view-book.component.ts
+++ src/app/view-book/view-book.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-book.component.css']
 })
 export class ViewBookComponent {
-
+  books:any = [
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    },
+    {
+      "title":"Coffee Guide",
+      "image":"https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "author":"Fernandes Richard",
+      "price":499
+    }
+  ];
 }
